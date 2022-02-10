52dcde394715060d9d0d6c91b9869f12dc8a45c1
Randy, Huang <gg875437@gmail.com>
2022-02-10 21:50:07 +0900

    enable loading image

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div>
+      {/* Left */}
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div>
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+
+      {/* Center */}
+
+      {/* Right */}
+    </div>
+  )
 }
 
 export default Header
