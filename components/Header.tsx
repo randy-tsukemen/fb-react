14b9eb29cf865f16c43a7a8b82385584527c5388
Randy, Huang <gg875437@gmail.com>
2022-02-10 21:59:17 +0900

    left part of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,37 @@
 import Image from 'next/image'
-import React from 'react'
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid'
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline'
 
 const Header = () => {
   return (
     <div>
       {/* Left */}
-      <div>
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
         />
-        <div>
-          <input type="text" placeholder="Search Facebook" />
+        <div className="ml-2 flex items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="ml-2 flex items-center bg-transparent placeholder-gray-500 outline-none"
+            type="text"
+            placeholder="Search Facebook"
+          />
         </div>
       </div>
 
