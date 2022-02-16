65996ce700fa7a2175bd5901c6bff1203d198f7c
Randy, Huang <gg875437@gmail.com>
2022-02-16 22:01:50 +0900

    add login components for login

---
+++ components/Login.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <Image
+        src="https://links.papareact.com/t4i"
+        width={400}
+        height={400}
+        alt="Facebook"
+        objectFit="contain"
+      />
+    </div>
+  )
+}
+
+export default Login
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/react'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />
   return (
     <div className="">
       <Head>
@@ -16,4 +19,14 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  // Get the user's session
+  const session = await getSession(context)
+  return {
+    props: {
+      session,
+    },
+  }
 }
